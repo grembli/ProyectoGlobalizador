d6c239b3f651e5c1d80e60bc937c109338b6a6f7
Jose <Jose@DESKTOP-6R6K26P>
2017-05-18 12:08:37 +0200

    Commit inicial

---
+++ ProyectoGlobalizador/src/proyectoglobalizador/ProyectoGlobalizador.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoglobalizador;
+
+/**
+ *
+ * @author daw112
+ */
+public class ProyectoGlobalizador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaPrincipal vp = new VentanaPrincipal();
+        vp.setVisible(true);
+    }
+    
+}
